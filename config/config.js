474d53393edd8495f73b5da2b8ba9e72517f1a70
Hemslo Wang <hemslo.wang@gmail.com>
2013-06-06 14:53:53 +0800

    fix google auth config

--- config/config.js
+++ config/config.js
@@ -22,11 +22,11 @@ module.exports = {
     github: {
       clientID: '5830cf0d80d4c9b8c62f',
       clientSecret: '168059ea4de1fdebeabe53106d515d835868c016',
-      callbackURL: 'http://geekstack.org/auth/github/callback'
+      callbackURL: 'http://geekon.geekstack.org/auth/github/callback'
     },
     google: {
-      returnURL: 'http://geekstack.org/auth/google/callback',
-      realm: 'http://geekstack.org'
+      returnURL: 'http://geekon.geekstack.org/auth/google/callback',
+      realm: 'http://geekon.geekstack.org'
     }
   }
 };
